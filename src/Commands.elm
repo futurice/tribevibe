b27d0753dc6583e454905d559d3325a8c4e0e456
Antti Mattila <anttti@fastmail.fm>
2018-01-19 21:31:11 +0200

    Simplify API usage

--- src/Commands.elm
+++ src/Commands.elm
@@ -1,7 +1,6 @@
 module Commands exposing (..)
 
 import Http
-import Json.Encode as Encode exposing (..)
 import Decoders exposing (decodeWeeklyReports)
 import Messages exposing (..)
 
@@ -9,29 +8,10 @@ import Messages exposing (..)
 getWeeklyReports : Cmd Msg
 getWeeklyReports =
     let
-        -- "https://app.officevibe.com/api/v2/engagement"
         url =
-            "http://localhost:3001/engagement"
-
-        body =
-            Http.jsonBody <|
-                Encode.object
-                    [ ( "groupNames", Encode.string "Tammerforce" )
-                    , ( "dates", Encode.list (List.map Encode.string [ "2017-12-01", "2018-01-01" ]) )
-                    ]
-
-        headers =
-            [ Http.header "Authorization" "Bearer TOKEN" ]
+            "http://localhost:3001/engagement?groupNames=Tammerforce&dates=2017-12-01,2018-01-01"
 
         request =
-            Http.request
-                { method = "POST"
-                , headers = headers
-                , url = url
-                , body = body
-                , expect = Http.expectJson decodeWeeklyReports
-                , timeout = Nothing
-                , withCredentials = False
-                }
+            Http.get url decodeWeeklyReports
     in
         Http.send ReceiveWeeklyReports request
