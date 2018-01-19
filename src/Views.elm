fa7bae96c04cb926a3d513419a196c372af28a72
Antti Mattila <anttti@fastmail.fm>
2018-01-19 21:59:50 +0200

    Filter only interesting values

--- src/Views.elm
+++ src/Views.elm
@@ -5,6 +5,11 @@ import Types exposing (WeeklyReport, MetricsValue, interestingMetrics)
 import Messages exposing (..)
 
 
+isInteresting : MetricsValue -> Bool
+isInteresting value =
+    List.member value.id interestingMetrics
+
+
 metricValues : List WeeklyReport -> Html Msg
 metricValues reports =
     ul [] (List.map viewReport reports)
@@ -15,7 +20,7 @@ viewReport report =
     li []
         [ p [] [ text report.groupName ]
         , p [] [ text report.date ]
-        , ul [] (List.map viewMetricsValue report.metricsValues)
+        , ul [] (List.map viewMetricsValue (List.filter isInteresting report.metricsValues))
         ]
 
 
