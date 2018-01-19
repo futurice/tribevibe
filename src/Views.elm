f3ee9dd552beb8561989343a24c4d28df4dfc2cf
Antti Mattila <anttti@fastmail.fm>
2018-01-19 23:28:25 +0200

    Beats & styles

--- src/Views.elm
+++ src/Views.elm
@@ -1,6 +1,7 @@
 module Views exposing (..)
 
-import Html exposing (Html, text, ul, li, p)
+import Html exposing (Html, text, ul, li, p, div)
+import Html.Attributes exposing (class)
 import Types exposing (WeeklyReport, MetricsValue, interestingMetrics)
 import Messages exposing (..)
 
@@ -10,22 +11,28 @@ isInteresting value =
     List.member value.id interestingMetrics
 
 
+withEmoji : Float -> String
+withEmoji value =
+    "😄 " ++ toString value
+
+
 metricValues : List WeeklyReport -> Html Msg
 metricValues reports =
-    ul [] (List.map viewReport reports)
+    ul [ class "weekly-reports" ] (List.map viewReport reports)
 
 
 viewReport : WeeklyReport -> Html Msg
 viewReport report =
-    li []
+    li [ class "weekly-report" ]
         [ p [] [ text report.groupName ]
         , p [] [ text report.date ]
-        , ul [] (List.map viewMetricsValue (List.filter isInteresting report.metricsValues))
+        , ul [ class "cards" ] (List.map viewMetricsValue (List.filter isInteresting report.metricsValues))
         ]
 
 
 viewMetricsValue : MetricsValue -> Html Msg
 viewMetricsValue metricsValue =
-    li []
-        [ text ((metricsValue.displayName ++ ": ") ++ toString metricsValue.value)
+    li [ class "card" ]
+        [ div [ class "card__header" ] [ text metricsValue.displayName ]
+        , div [ class "card__body" ] [ text (withEmoji metricsValue.value) ]
         ]
