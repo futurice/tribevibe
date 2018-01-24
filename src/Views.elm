2ec6b75fc50157ecc8632c0cfebfd31aa2b0b0cb
Antti Mattila <anttti@fastmail.fm>
2018-01-24 20:33:02 +0200

    Remove redundant Http.Attributes.

--- src/Views.elm
+++ src/Views.elm
@@ -42,52 +42,52 @@ viewDashboard dashboard =
             p [] [ text "Loading..." ]
 
         Just dashboard ->
-            div [ Html.Attributes.class "dashboard" ]
-                [ div [ Html.Attributes.class "dashboard__top" ]
-                    [ div [ Html.Attributes.class "card card--big card--graph" ] [ viewGraph (List.head (List.filter onlyEngagement dashboard.metrics)) ]
-                    , div [ Html.Attributes.class "card card--big card--engagement" ] [ viewEngagements dashboard.engagements ]
-                    , div [ Html.Attributes.class "card card--big card--feedback" ] (List.map viewFeedback dashboard.feedbacks)
+            div [ class "dashboard" ]
+                [ div [ class "dashboard__top" ]
+                    [ div [ class "card card--big card--graph" ] [ viewGraph (List.head (List.filter onlyEngagement dashboard.metrics)) ]
+                    , div [ class "card card--big card--engagement" ] [ viewEngagements dashboard.engagements ]
+                    , div [ class "card card--big card--feedback" ] (List.map viewFeedback dashboard.feedbacks)
                     ]
-                , ul [ Html.Attributes.class "dashboard__bottom" ] (List.map viewMetric (List.filter allButEngagement dashboard.metrics))
+                , ul [ class "dashboard__bottom" ] (List.map viewMetric (List.filter allButEngagement dashboard.metrics))
                 ]
 
 
 viewMetric : Metric -> Html Msg
 viewMetric metric =
-    li [ Html.Attributes.class "card" ]
-        [ div [ Html.Attributes.class "card__header" ] [ text metric.name ]
-        , div [ Html.Attributes.class "card__body" ] [ text (lastWithEmoji metric.values) ]
+    li [ class "card" ]
+        [ div [ class "card__header" ] [ text metric.name ]
+        , div [ class "card__body" ] [ text (lastWithEmoji metric.values) ]
         ]
 
 
 viewFeedback : Feedback -> Html Msg
 viewFeedback feedback =
     div []
-        [ h3 [ Html.Attributes.class "card__title" ] [ text feedback.question ]
-        , p [ Html.Attributes.class "feedback__answer" ] [ text feedback.answer ]
-        , h3 [ Html.Attributes.class "feedback__replies-title" ] [ text "Replies" ]
-        , ul [ Html.Attributes.class "feedback__replies" ] (List.map viewReply feedback.replies)
+        [ h3 [ class "card__title" ] [ text feedback.question ]
+        , p [ class "feedback__answer" ] [ text feedback.answer ]
+        , h3 [ class "feedback__replies-title" ] [ text "Replies" ]
+        , ul [ class "feedback__replies" ] (List.map viewReply feedback.replies)
         ]
 
 
 viewReply : Reply -> Html Msg
 viewReply reply =
-    li [ Html.Attributes.class "feedback__reply" ] [ text reply.message ]
+    li [ class "feedback__reply" ] [ text reply.message ]
 
 
 viewEngagements : Engagements -> Html Msg
 viewEngagements engagements =
     div []
-        [ h3 [ Html.Attributes.class "card__title" ] [ text "Top Engagement" ]
-        , ul [ Html.Attributes.class "engagements" ] (List.indexedMap viewEngagement engagements)
+        [ h3 [ class "card__title" ] [ text "Top Engagement" ]
+        , ul [ class "engagements" ] (List.indexedMap viewEngagement engagements)
         ]
 
 
 viewEngagement : Int -> Engagement -> Html Msg
 viewEngagement index engagement =
-    li [ Html.Attributes.class "engagements-item" ]
+    li [ class "engagements-item" ]
         [ div []
-            [ span [ Html.Attributes.class "engagements-item__index" ] [ text ((toString (index + 1)) ++ ".") ]
+            [ span [ class "engagements-item__index" ] [ text ((toString (index + 1)) ++ ".") ]
             , span [] [ text engagement.name ]
             ]
         , div [] [ text (toString engagement.value) ]
