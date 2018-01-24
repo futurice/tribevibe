9df310bfb4e0dabf124b71cb419b01c50ceea98d
Mikko Pohja <mikko.pohja@futurice.com>
2018-01-24 12:44:04 +0200

    My first elm algorithm

--- src/Views.elm
+++ src/Views.elm
@@ -2,6 +2,9 @@ module Views exposing (..)
 
 import Html exposing (Html, text, ul, li, h2, h3, p, div)
 import Html.Attributes exposing (class)
+import Svg exposing (Svg)
+import Svg.Attributes exposing (..)
+import Plot exposing (..)
 import Types exposing (..)
 import Messages exposing (..)
 
@@ -28,6 +31,11 @@ allButEngagement metric =
     metric.id /= "Engagement"
 
 
+onlyEngagement : Metric -> Bool
+onlyEngagement metric =
+    metric.id == "Engagement"
+
+
 viewDashboard : Maybe Dashboard -> Html Msg
 viewDashboard dashboard =
     case dashboard of
@@ -35,36 +43,106 @@ viewDashboard dashboard =
             p [] [ text "Loading..." ]
 
         Just dashboard ->
-            div [ class "dashboard" ]
-                [ div [ class "dashboard__top" ]
-                    [ div [ class "card card--big" ] [ text "Here lives the graph!" ]
-                    , div [ class "card card--big" ] [ viewFeedback dashboard.feedback ]
+            div [ Html.Attributes.class "dashboard" ]
+                [ div [ Html.Attributes.class "dashboard__top" ]
+                    [ div [ Html.Attributes.class "card card--big" ] [ viewGraph (List.head (List.filter onlyEngagement dashboard.metrics)) ]
+                    , div [ Html.Attributes.class "card card--big" ] [ viewFeedback dashboard.feedback ]
                     ]
-                , ul [ class "dashboard__bottom" ] (List.map viewMetric (List.filter allButEngagement dashboard.metrics))
+                , ul [ Html.Attributes.class "dashboard__bottom" ] (List.map viewMetric (List.filter allButEngagement dashboard.metrics))
                 ]
+
+
+horizontalAxis : Metric -> Axis
+horizontalAxis metric =
+    normalAxis
+
+
+axisColor : String
+axisColor =
+    "#afafaf"
+
+
+title : Svg msg
+title =
+    viewLabel
+        [ fill axisColor
+        , style "text-anchor: end; font-style: italic;"
+        ]
+        "f(x) = sin x"
+
+
+viewGraph : Maybe Metric -> Html Msg
+viewGraph metric =
+    case metric of
+        Nothing ->
+            text "Nothing here"
+
+        Just metric ->
+            viewSeriesCustom
+                { defaultSeriesPlotCustomizations
+                    | toDomainLowest = \y -> y - 0.25
+                }
+                [ customLine ]
+                (List.indexedMap
+                    (\i x -> ( toFloat i, x ))
+                    metric.values
+                )
+
+
+pinkStroke : String
+pinkStroke =
+    "#ff9edf"
+
+
+verticalAxis : Axis
+verticalAxis =
+    customAxis <|
+        \summary ->
+            { position = Basics.min
+            , axisLine = Just (dataLine summary)
+            , ticks = List.map simpleTick (interval 0 0.5 summary)
+            , labels = List.map simpleLabel (interval 0 0.5 summary)
+            , flipAnchor = False
+            }
+
+
+dataLine : AxisSummary -> LineCustomizations
+dataLine summary =
+    { attributes = [ stroke "grey" ]
+    , start = summary.dataMin
+    , end = summary.dataMax
+    }
+
+
+customLine : Series (List ( Float, Float )) msg
+customLine =
+    { axis = verticalAxis
+    , interpolation = Monotone Nothing [ stroke pinkStroke ]
+    , toDataPoints = List.map (\( x, y ) -> clear x y)
+    }
 
 
 viewMetric : Metric -> Html Msg
 viewMetric metric =
-    li [ class "card" ]
-        [ div [ class "card__header" ] [ text metric.name ]
-        , div [ class "card__body" ] [ text (lastWithEmoji metric.values) ]
+    li [ Html.Attributes.class "card" ]
+        [ div [ Html.Attributes.class "card__header" ] [ text metric.name ]
+        , div [ Html.Attributes.class "card__body" ] [ text (lastWithEmoji metric.values) ]
         ]
 
 
 viewFeedback : Feedback -> Html Msg
 viewFeedback feedback =
-    div [ class "feedback" ]
-        [ h2 [ class "feedback__question" ] [ text feedback.question ]
-        , p [ class "feedback__answer" ] [ text feedback.answer ]
-        , h3 [ class "feedback__replies-title" ] [ text "Replies" ]
-        , ul [ class "feedback__replies" ] (List.map viewReply feedback.replies)
+    div [ Html.Attributes.class "feedback" ]
+        [ h2 [ Html.Attributes.class "feedback__question" ] [ text feedback.question ]
+        , p [ Html.Attributes.class "feedback__answer" ] [ text feedback.answer ]
+        , h3 [ Html.Attributes.class "feedback__replies-title" ] [ text "Replies" ]
+        , ul [ Html.Attributes.class "feedback__replies" ] (List.map viewReply feedback.replies)
         ]
 
 
 viewReply : Reply -> Html Msg
 viewReply reply =
-    li [ class "feedback__reply" ] [ text reply.message ]
+    li [ Html.Attributes.class "feedback__reply" ] [ text reply.message ]
 
 
 viewError : Maybe String -> Html Msg
